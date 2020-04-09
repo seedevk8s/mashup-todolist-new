e4554b2590c0a7aa8a76e4b03ed3da21531507a4
seedevk8s <seedevk8s@gmail.com>
2020-04-09 18:05:26 +0900

    TodoItem 만들기--이전까지

---
+++ src/components/TodoItem.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styled, { css } from "styled-components";
+
+function TodoItem() {
+  return (
+    // eslint-disable-next-line react/jsx-filename-extension
+    <div></div>
+  );
+}
+
+export default TodoItem;
