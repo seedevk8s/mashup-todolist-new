dcf74f5fc3bbb40839ae7ff7abd25d972d4154b0
seedevk8s <seedevk8s@gmail.com>
2020-04-12 14:32:30 +0900

    TodoItem 만들기-1

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,10 +1,48 @@
 import React from "react";
 import styled, { css } from "styled-components";
+import { MdDone, MdDelete } from "react-icons/md";
 
-function TodoItem() {
+const Remove = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #dee2e6;
+  font-size: 24px;
+  cursor: pointer;
+  &:hover {
+    color: #ff6b6b;
+  }
+  display: none;
+`;
+
+const TodoItemBlock = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 12px;
+  padding-bottom: 12px;
+  &:hover {
+    ${Remove} {
+      display: initial;
+    }
+  }
+`;
+
+const Text = styled.div`
+  flex: 1;
+  font-size: 21px;
+  color: #495057;
+  ${(props) =>
+    props.done &&
+    css`
+      color: #ced4da;
+    `}
+`;
+
+function TodoItem({ id, text, done }) {
   return (
-    // eslint-disable-next-line react/jsx-filename-extension
-    <div></div>
+    <TodoItemBlock>
+      <Text done={done}>{text}</Text>
+    </TodoItemBlock>
   );
 }
 
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,19 +1,24 @@
 import React from "react";
 import styled from "styled-components";
+import TodoItem from "./TodoItem";
 
 const TodoListBlock = styled.div`
   flex: 1;
   padding: 20px 32px;
   padding-bottom: 48px;
   overflow-y: auto;
-  background: gray; /* 사이즈 조정이 잘 되고 있는지 확인하기 위한 임시 스타일 */
 `;
 
 function TodoList() {
   return (
     // eslint-disable-next-line react/jsx-filename-extension
     <div>
-      <TodoListBlock>TodoList</TodoListBlock>
+      <TodoListBlock>
+        <TodoItem text="프로젝트 대비" done={true} />
+        <TodoItem text="리액트 파악 정리" done={true} />
+        <TodoItem text="자바 fc 내용 파악 정리" done={false} />
+        <TodoItem text="node.js 수강여부 판단" done={false} />
+      </TodoListBlock>
     </div>
   );
 }
