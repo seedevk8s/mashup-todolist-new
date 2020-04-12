454da7cdf2c607b1d3b32589cfa694f0aecd31b5
seedevk8s <seedevk8s@gmail.com>
2020-04-12 14:54:44 +0900

    TodoItem 만들기-2

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -38,9 +38,29 @@ const Text = styled.div`
     `}
 `;
 
+const CheckCircle = styled.div`
+  width: 32px;
+  height: 32px;
+  border-radius: 16px;
+  border: 1px solid #ced4da;
+  font-size: 24px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+  cursor: pointer;
+  ${(props) =>
+    props.done &&
+    css`
+      border: 1px solid #38d9a9;
+      color: #38d9a9;
+    `}
+`;
+
 function TodoItem({ id, text, done }) {
   return (
     <TodoItemBlock>
+      <CheckCircle done={done}>{done && <MdDone />}</CheckCircle>
       <Text done={done}>{text}</Text>
     </TodoItemBlock>
   );
